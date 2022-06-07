80eb8afcaeffa01d787a04d24d956c75e6f810f6
Kleber Lucas de Santana Costa <55465681+kleber-code@users.noreply.github.com>
2022-06-07 20:07:46 -0300

    ups

--- script/game.js
+++ script/game.js
@@ -1,38 +1,52 @@
 const elementIds = [
-    ['c0l0','c0l1','c0l2','c0l3','c0l4','c0l5','c0l6','c0l7','c0l8','c0l9'],
-    ['c1l0','c1l1','c1l2','c1l3','c1l4','c1l5','c1l6','c1l7','c1l8','c1l9'],
-    ['c2l0','c2l1','c2l2','c2l3','c2l4','c2l5','c2l6','c2l7','c2l8','c2l9'],
-    ['c3l0','c3l1','c3l2','c3l3','c3l4','c3l5','c3l6','c3l7','c3l8','c3l9'],
-    ['c4l0','c4l1','c4l2','c4l3','c4l4','c4l5','c4l6','c4l7','c4l8','c4l9'],
-    ['c5l0','c5l1','c5l2','c5l3','c5l4','c5l5','c5l6','c5l7','c5l8','c5l9'],
-    ['c6l0','c6l1','c6l2','c6l3','c6l4','c6l5','c6l6','c6l7','c6l8','c6l9'],
-    ['c7l0','c7l1','c7l2','c7l3','c7l4','c7l5','c7l6','c7l7','c7l8','c7l9'],
-    ['c8l0','c8l1','c8l2','c8l3','c8l4','c8l5','c8l6','c8l7','c8l8','c8l9'],
-    ['c9l0','c9l1','c9l2','c9l3','c9l4','c9l5','c9l6','c9l7','c9l8','c9l9']
+    ['c0l0','c0l1','c0l2','c0l3','c0l4','c0l5'],
+    ['c1l0','c1l1','c1l2','c1l3','c1l4','c1l5'],
+    ['c2l0','c2l1','c2l2','c2l3','c2l4','c2l5'],
+    ['c3l0','c3l1','c3l2','c3l3','c3l4','c3l5'],
+    ['c4l0','c4l1','c4l2','c4l3','c4l4','c4l5'],
+    ['c5l0','c5l1','c5l2','c5l3','c5l4','c5l5'],
 ]
-var playerMap = [
-    ['@','','#','#','#','#','#','#','#','#'],
-    ['','','#','#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#','#','#','#'],
-    ['#','#','#','#','#','#','#','#','#','E']
+const map = [
+    [
+        ['@','','','','',''],
+        ['','','','','',''],
+        ['','','','','',''],
+        ['','','','','',''],
+        ['','','','','',''],
+        ['','','','','','X'],
+    ],
+    [
+        ['#','#','#','#','#','#'],
+        ['@','','','','',''],
+        ['','','','','',''],
+        ['','','','','',''],
+        ['','','','','','X'],
+        ['#','#','#','#','#','#'],
+    ],
+    [
+        ['#','#','#','#','#','#'],
+        ['#','@','','','','#'],
+        ['#','','','','','#'],
+        ['#','','','','','#'],
+        ['#','','','','X','#'],
+        ['#','#','#','#','#','#'],
+    ],
+    [
+        ['@','','','','',''],
+        ['#','#','#','','#','#'],
+        ['','','','','',''],
+        ['','#','#','#','#','#'],
+        ['','','','','',''],
+        ['#','#','#','#','#','X'],
+    ]
 ]
 var gameMap = [
-    ['@','','','','','','','','',''],
-    ['','','','','','Aa','','','',''],
-    ['','','','','','','','','aa',''],
-    ['','','','','','','','','',''],
-    ['','','Aa','','','','','','',''],
-    ['','','','','','','','','',''],
-    ['','','','','','','','','',''],
-    ['','','','','','AA','','','',''],
-    ['','','','','','','','','',''],
-    ['','','','','','','','','','X']
+    ['@','','','','',''],
+    ['','','','','',''],
+    ['','','','','',''],
+    ['','','','','',''],
+    ['','','','','',''],
+    ['','','','','','X'],
 ]
 
 mosterQt = {
@@ -45,7 +59,8 @@ aaTime = 0;
 
 playerTurns = 0;
 
-var player = [0,0]
+var player = [0,0];
+var playerInSpawn = false;
 
 document.addEventListener ('keypress', (event) => {
     const oldPlayer = [player[0],player[1]];
@@ -58,7 +73,7 @@ document.addEventListener ('keypress', (event) => {
             }
             break;
         case ('s'):
-            if (player[0] < 9) {    
+            if (player[0] < gameMap.length - 1) {    
                 player[0] += 1;
             }
             break;
@@ -68,7 +83,7 @@ document.addEventListener ('keypress', (event) => {
             }
             break;
         case ('d'):
-            if (player[1] < 9) {    
+            if (player[1] < gameMap[0].length -1) {    
                 player[1] += 1;
             }
             break;
@@ -85,15 +100,53 @@ function game (oldPlayerPos) {
     playerChecker(oldPlayerPos);
     draw();
     enemyChecker();
-    draw();   
+    draw();
 
-    function playerChecker(oldPlayerPos) {
-        if (gameMap[oldPlayerPos[0]][oldPlayerPos[1]] == '@'){
+    function generatorNewMap () {
+        const d = Math.floor(Math.random()*4);
+        switch (d) {
+            case 0:
+                gameMap = [[]];
+                gameMap = map[0];
+                player = [0,0];
+                gameMap[player[0]][player[1]] = '@';
+                playerInSpawn = true;
+                break;
+            case 1:
+                gameMap=[[]];
+                gameMap = map[1];
+                player = [1,0];
+                gameMap[player[0]][player[1]] = '@';
+                playerInSpawn = true;
+                break;
+            case 2:
+                gameMap=[[]];
+                gameMap = map[2];
+                player = [1,1];
+                gameMap[player[0]][player[1]] = '@';
+                playerInSpawn = true;
+                break;
+            case 3:
+                gameMap=[[]];
+                gameMap = map[3];
+                player = [0,0];
+                gameMap[player[0]][player[1]] = '@';
+                playerInSpawn = true;
+                break;
+        }
+    }
+
+    function playerChecker (oldPlayerPos) {
+        if (gameMap[oldPlayerPos[0]][oldPlayerPos[1]] == '@' || playerInSpawn){
             switch (gameMap[player[0]][player[1]]) {
+                case '#':
+                    player = oldPlayerPos;
+                    break;
                 case '@':
                 case '':
                     gameMap[oldPlayerPos[0]][oldPlayerPos[1]] = '';
                     gameMap[player[0]][player[1]] = '@';
+                    playerInSpawn = false;
                     break;
                 case 'AA':
                 case 'Aa':
@@ -101,8 +154,9 @@ function game (oldPlayerPos) {
                     gameover()
                     break;
                 case 'X':
-                    alert('Parabéns você sobreviveu!')
-                    location.reload();
+                    gameMap[oldPlayerPos[0]][oldPlayerPos[1]] = '';
+                    generatorNewMap();
+                    draw()
                     break;
             }
         } else {
@@ -199,8 +253,8 @@ function game (oldPlayerPos) {
     }
 
     function draw () {
-        for (var y = 0; y < 10; y++) {
-            for (var x = 0; x < 10; x++) {
+        for (var y = 0; y < gameMap.length; y++) {
+            for (var x = 0; x < gameMap[y].length; x++) {
                 const id = elementIds[y][x];
                 document.getElementById(id).innerHTML = gameMap[y][x];
                 if (gameMap[y][x] == '@'){
